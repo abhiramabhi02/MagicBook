e677745ec96f8e8ca84737912d7b84e782671a84
ABHI <abhiramabhinv2@gmail.com>
2024-05-08 00:58:27 +0530

    com

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 mongoose.set('strictQuery', false);
-const connectdb =  mongoose.connect("mongodb+srv://abhiramabhinv2:Xpulse200@cluster0.7zeyv1z.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true})
+const connectdb =  mongoose.connect('mongodb://0.0.0.0:27017/magicbook')
+
+const dbcon = ["mongodb+srv://abhiramabhinv2:Xpulse200@cluster0.7zeyv1z.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true}]
 
 module.exports={
     connectdb
